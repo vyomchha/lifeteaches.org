b45f4f70a3a9ca323bc4244dc828e88ab867eb2b
Lija Jose <lija.m.j@gmail.com>
2017-12-02 08:00:53 -0600

    Initial commit
    Added organization page and related files.
    Added form validation for Safari.
    Added custom select drop down arrow.

---
+++ js/js_org.js
@@ -0,0 +1,24 @@
+﻿/****************************
+	Author: Lija Jose
+	Date  : 11/13/2017 Log: Created
+
+*****************************/
+
+var showLess = false;
+
+$(document).ready(function () {    
+    $('#org_show_more_info').hide();    
+    $('#org_show_more_info').load("content/brac_show_more.html");
+});
+
+function show_more_onclick() {
+    showLess = !showLess;
+    $('#org_show_more_info').slideToggle();
+    if(showLess)
+    {
+        $('.org_show_more_button').html("Show <br> Less");
+    }
+    else {
+        $('.org_show_more_button').html("Show <br> More");
+    }
+}
